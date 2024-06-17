cbbd5763158e5b9a3a5c78e57e3707db7cfb2aeb
caioorleans <caioorleansdm@gmail.com>
2024-06-17 10:57:34 -0300

    Refactored EmailTextField and PasswordTextField

--- app/src/main/java/com/example/appcursos/components/PasswordTextField.kt
+++ app/src/main/java/com/example/appcursos/components/PasswordTextField.kt
@@ -1,9 +1,9 @@
 package com.example.appcursos.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -13,28 +13,35 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.appcursos.ui.theme.gray1
 import com.example.appcursos.ui.theme.gray2
+import com.example.appcursos.ui.theme.primary
 import com.example.appcursos.ui.theme.secondary
 
 @Composable
-fun EmailTextField(modifier: Modifier = Modifier){
-    var email by remember { mutableStateOf("") }
+fun PasswordTextField(modifier: Modifier = Modifier){
+    var password by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(true) }
     OutlinedTextField(
-        value = email,
-        onValueChange = {email = it},
+        value = password,
+        onValueChange = {password = it},
         label = {
-            Text(text = "Email")
+            Text(text = "Password")
         },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Email
-        ),
+        trailingIcon = {
+            Text(
+                text = if(showPassword)  "Hide" else "Show",
+                color = primary,
+                modifier =
+                    Modifier
+                        .padding(end = 15.dp)
+                        .clickable { showPassword = !showPassword }
+            ) },
         colors = OutlinedTextFieldDefaults.colors(
             unfocusedContainerColor = secondary,
             unfocusedBorderColor = gray1,
-            unfocusedTextColor = gray2
+            unfocusedTextColor = gray2,
         ),
         shape = RoundedCornerShape(10.dp),
         modifier = Modifier
--- app/src/main/java/com/example/appcursos/screens/Login.kt
+++ app/src/main/java/com/example/appcursos/screens/Login.kt
@@ -12,9 +12,11 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,37 +24,33 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appcursos.components.EmailTextField
+import com.example.appcursos.components.PasswordTextField
 import com.example.appcursos.ui.theme.AppCursosTheme
+import com.example.appcursos.ui.theme.gray1
+import com.example.appcursos.ui.theme.gray2
+import com.example.appcursos.ui.theme.primary
+import com.example.appcursos.ui.theme.secondary
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+
+
     Scaffold(
         topBar = {TopAppBarLogin()}
     ) {
         Column(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier.padding(it)
+            modifier = modifier
+                .padding(it)
+                .padding(10.dp)
         ){
-            OutlinedTextField(
-                value = email,
-                onValueChange = {email = it},
-                label = {
-                    Text(text = "Email")
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = password,
-                onValueChange = {password = it},
-                label = {
-                    Text(text = "Password")
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
+            EmailTextField()
+            PasswordTextField()
             Button(
                 onClick = { /*TODO*/ },
                 modifier = Modifier.fillMaxWidth()
