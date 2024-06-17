18abbaab074abcdc59308299769114c0ef396d0a
Caio Orleans <caioorleansdm@gmail.com>
2024-06-17 20:51:13 -0300

    Added background color

--- app/src/main/java/com/example/appcursos/ui/theme/Theme.kt
+++ app/src/main/java/com/example/appcursos/ui/theme/Theme.kt
@@ -9,18 +9,21 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = primary,
     secondary = secondary,
-    tertiary = tertiary
+    tertiary = tertiary,
+    background = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = primary,
     secondary = secondary,
-    tertiary = tertiary
+    tertiary = tertiary,
+    background = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
