d9e91e0b39a19d0fd4fd2eb9f87ad3e7b8c97b07
caioorleans <caioorleansdm@gmail.com>
2024-06-17 10:56:28 -0300

    Added new colors

--- app/src/main/java/com/example/appcursos/ui/theme/Color.kt
+++ app/src/main/java/com/example/appcursos/ui/theme/Color.kt
@@ -5,3 +5,6 @@ import androidx.compose.ui.graphics.Color
 val primary = Color(0xFF5DB075)
 val secondary = Color(0xFFE8E8E8)
 val tertiary = Color(0xFF000000)
+
+val gray1 = Color(0xFFE8E8E8)
+val gray2 = Color(0xFFBDBDBD)
