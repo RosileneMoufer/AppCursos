8c80d8199eedb648882dac2d1819d6d572815168
RosileneMoufer <rosilenemoufer@gmail.com>
2024-06-19 11:50:04 -0300

    PostList update

--- app/src/main/java/com/example/appcursos/components/PostList.kt
+++ app/src/main/java/com/example/appcursos/components/PostList.kt
@@ -1,17 +1,35 @@
 package com.example.appcursos.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun PostList() {
+fun PostList(list: List<String>) {
+    /*
+    if (list.isEmpty()) {
+        Text(text = "Ainda não há nada")
+    } else {
+        list.let {
+            LazyColumn {
+                items(it) {
+                    Card("Java", "8m ago","description: String")
+                }
+            }
+        }
+    }
+*/
+
     Column {
         Card("Java", "8m ago","description: String")
         Card("Python", "8m ago", "description: String")
         Card("Go", "8m ago", "description: String")
         Card("C#", "8m ago", "description: String")
     }
+
 }
